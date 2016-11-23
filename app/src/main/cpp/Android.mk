7323bc9d27c790ecf53b605b42fb892b5ae1aa3f
李樟取 <lizhangqu@weidian.com>
2016-11-23 23:03:24 +0800

    google 拼音输入法 gradle 编译通过

---
+++ app/src/main/cpp/Android.mk
@@ -0,0 +1,34 @@
+LOCAL_PATH := $(call my-dir)
+
+### shared library
+
+include $(CLEAR_VARS)
+
+LOCAL_SRC_FILES := \
+	android/com_android_inputmethod_pinyin_PinyinDecoderService.cpp \
+	share/dictbuilder.cpp \
+	share/dictlist.cpp \
+	share/dicttrie.cpp \
+	share/lpicache.cpp \
+	share/matrixsearch.cpp \
+	share/mystdlib.cpp \
+	share/ngram.cpp \
+	share/pinyinime.cpp \
+	share/searchutility.cpp \
+	share/spellingtable.cpp \
+	share/spellingtrie.cpp \
+	share/splparser.cpp \
+	share/userdict.cpp \
+	share/utf16char.cpp \
+	share/utf16reader.cpp \
+	share/sync.cpp
+
+LOCAL_C_INCLUDES += $(JNI_H_INCLUDE)
+LOCAL_MODULE := libjni_pinyinime
+#LOCAL_SHARED_LIBRARIES := libcutils libutils
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_CFLAGS += -D_PLATFORM_ANDROID
+LOCAL_LDLIBS := -llog -landroid -lz
+
+include $(BUILD_SHARED_LIBRARY)
